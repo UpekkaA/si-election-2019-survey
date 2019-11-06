0a02c1bd53fa32bf16db8d5a46b620ad3e67c0e3
UpekkaA <upekka.12@cse.mrt.ac.lk>
2019-11-06 23:03:44 +0530

    Data plots added

---
+++ scripts/data_plots_distributions.R
@@ -0,0 +1,40 @@
+# library
+library(ggplot2)
+
+# load dataset
+electionData <- read.csv(file="../responses/responses_preprocessed_2.csv", header=TRUE, sep=",")
+candidate <- table(electionData$Voted_Candidate)
+
+# Stacked
+#ggplot(electionData, aes(fill=candidate, y=candidate, x=education)) + 
+  #geom_bar(position="stack", stat="identity")
+
+# Age Distribution
+ages <- table(electionData$Age)
+barplot(ages, main="Age Distribution", col=c("darkblue"),
+        xlab="Age",ylab="Frequency")
+
+# Education Distribution
+education <- table(electionData$Highest_Educational_Qualification)
+barplot(education, main="Education Distribution", col=c("darkblue"),
+        xlab="Education",ylab="Frequency", cex.names=0.8 , las=2)
+
+# Religion Distribution
+Religion<- table(electionData$Religion)
+barplot(Religion, main="Religion Distribution", col=c("darkblue"),
+        xlab="Religion",ylab="Frequency", cex.names=0.8)
+
+# Race Distribution
+Race <- table(electionData$Race)
+barplot(Race, main="Race Distribution", col=c("darkblue"),
+        xlab="Race",ylab="Frequency", cex.names=0.8)
+
+# Occupation Distribution
+Occupation <- table(electionData$Occupation)
+barplot(Occupation, main="Occupation Distribution", col=c("darkblue"),
+        xlab="Occupation",ylab="Frequency", cex.names=0.8 , las=2)
+
+# Distrct Distribution
+District<- table(electionData$District)
+barplot(District, main="District Distribution", col=c("darkblue"),
+        xlab="District",ylab="Frequency", cex.names=0.8 , las=2)
--- scripts/data_plots_pie.R
+++ scripts/data_plots_pie.R
@@ -1,14 +1,27 @@
 # Read CSV into R
-electionData <- read.csv(file="F:/SI/survey/si-election-2019-survey/responses/responses_preprocessed_2.csv", header=TRUE, sep=",")
+electionData <- read.csv(file="../responses/responses_preprocessed_2.csv", header=TRUE, sep=",")
 #head(electionData)
-mytable <- table(electionData$Voted_Candidate)
+winnertable <- table(electionData$Voted_Candidate)
 
 #plot pie chart for the voted candidates
-pct <- round(mytable/sum(mytable)*100,2)
-lbls <- paste(names(mytable),"\n", pct) # add percents to labels
+pct <- round(winnertable/sum(winnertable)*100,2)
+lbls <- paste(names(winnertable),"\n", pct) # add percents to labels
 lbls <- paste(lbls,"%",sep="") # ad % to labels
 colors <- c('red', 'yellow', 'blue', 'pink','brown','green','violet')
-pie(mytable, main="Voted Candidates",labels = lbls, col=colors)
+pie(winnertable, main="Voted Candidates",labels = lbls, col=colors)
+
+
+partytable <- table(electionData$Winning_Party_Guess)
+
+#plot pie chart for the opinion on the most likely party to win the Presidential Election
+pct2 <- round(partytable/sum(partytable)*100,2)
+lbls2 <- paste(names(partytable), pct2) # add percents to labels
+lbls2 <- paste(lbls2,"%",sep="") # ad % to labels
+colors2 <- c('yellow','red', 'green', 'pink','blue','brown','violet')
+pie(partytable, main="Opinion on the Most Likely Party to Win the Presidential Election",labels = lbls2, col=colors2)
+
+
+
 
 
 
